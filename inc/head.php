2b85193e4d5a5d10763cee80317e1a84d1a1c38a
LucasBella <lucas@belladasemana.com.br>
2018-11-11 22:48:16 +0000

    remove unnecessary and unused files

--- inc/head.php
+++
@@ -1,7 +0,0 @@
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="Keywords" content="Ghost-Route, Ghost Route, rota de redes software, mapeamento de redes, lista de redes">
-<meta name="Description" content="Software Destinado a cadastros e listagens de Redes sem fio">
-<meta name="author" content="Lucas Marques Dutra - dutra.zero@gmail.com">
-<link rel="icon" href="/img/favicon.ico" type="image/x-icon">
--- inc/scripts.php
+++
@@ -1,3 +0,0 @@
-<script src="js/jquery.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="plugins/jquery/jquery.mask.min.js"></script>
